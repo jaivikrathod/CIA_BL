98c306299f76403b44066012d51a6155e172aba0
jaivikrathod <jaivikrathod7@gmail.com>
2025-04-11 00:51:30 +0530

    fix

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
-
 const mysql = require('mysql2/promise'); // Ensure you are using promise-based connection
+require('dotenv').config(); // Load environment variables from .env file
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'care_insurance',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 
