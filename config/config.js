fdded0ed25eb1ecf151004e62b23aa38a1248c6e
jaivikrathod <jaivikrathod7@gmail.com>
2025-07-29 22:25:19 +0530

    final

--- config/config.js
+++ config/config.js
@@ -2,10 +2,17 @@ require('dotenv').config(); // Load .env
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   host: process.env.DB_HOST,
+  //   dialect: 'mysql'
+  // },
+    username: 'root',
+    password: 'gHqDquZDbraLJsjrBnTRYqrGUUBcCbWq',
+    database: 'railway',
+    host:'shinkansen.proxy.rlwy.net',
+    port:'43550',
     dialect: 'mysql'
   },
   test: {
