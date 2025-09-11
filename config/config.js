fb33aa3d4944952ebfc61fa30939692cdddddde0
jaivikrathod <jaivikrathod7@gmail.com>
2025-09-12 00:55:18 +0530

    fix

--- config/config.js
+++ config/config.js
@@ -1,22 +1,22 @@
 require('dotenv').config(); // Load .env
 
 module.exports = {
-  // development: {
-  //   username: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME,
-  //   host: process.env.DB_HOST,
-  //   dialect: 'mysql',
-  //   port: '3306'
-  // },
   development: {
-    username: 'jaivikrathod',
-    password: '9844@Mysql',
-    database: 'care_insurance',
-    host: 'localhost',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: '3306'
   },
+  // development: {
+  //   username: 'jaivikrathod',
+  //   password: '9844@Mysql',
+  //   database: 'care_insurance',
+  //   host: 'localhost',
+  //   dialect: 'mysql',
+  //   port: '3306'
+  // },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
