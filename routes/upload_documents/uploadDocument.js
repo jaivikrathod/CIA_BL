1f471c3bc3dbd0715fef2c1383de33bc967c6485
jaivikrathod <jaivikrathod7@gmail.com>
2025-09-25 00:50:42 +0530

    fix

--- routes/upload_documents/uploadDocument.js
+++ routes/upload_documents/uploadDocument.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 25 * 1024 * 1024 }, 
+    limits: { fileSize: 100 * 1024 * 1024 }, 
     fileFilter: (req, file, cb) => {
         const allowedTypes = ['application/pdf', 'image/jpeg', 'image/png'];
         if (allowedTypes.includes(file.mimetype)) {
--- server.js
+++ server.js
@@ -3,26 +3,23 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = 3005;
-
 const routes = require('./routes/routes');
-// Middleware to enable CORS
+
 app.use(cors({
-    origin: '*',
-    // origin: 'https://jaivikrathod.github.io/',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization','X-User-ID','adminType']
+  origin: '*', 
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-User-ID', 'adminType']
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
-// Routes
 app.use('/api', routes);
 
 app.get("/api", (req, res) => {
-    res.send("API is working!");
-  });
+  res.send("API is working!");
+});
 
-// Start the server
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${port}`);
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
